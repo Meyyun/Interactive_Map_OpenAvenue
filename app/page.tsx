6bb7e7a1d9f075c49065151733b5f6a23caed1be
Sreileak Theap <163486314+Meyyun@users.noreply.github.com>
2026-02-22 22:13:47 -0500

    Wrap MapContainer in Suspense boundary to fix useSearchParams error

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import { Suspense } from 'react';
 import MapContainer from './components/Map/MapContainer';
 import { TaxAssessors } from './apollo/TaxAssessors';
+
 export default function Home() {
   return (
     <div>
-      <MapContainer />
+      <Suspense fallback={<div>Loading map...</div>}>
+        <MapContainer />
+      </Suspense>
       <h1>Welcome to Interactive Map</h1>
       <p>This is the home page of your interactive map application.</p>
       <TaxAssessors />
